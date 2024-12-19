92ac04fa0531ff07d3038cf4930e52e52f0f745c
Anzhelika Daudysh <anzhdaudysh@Anzhelikas-MacBook-Pro.local>
2024-12-19 18:58:20 +0300

    propotype react-app

---
+++ backend/index.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient } = require('mongodb');
+const db = require('./database');
+const recipeRoutes = require('./routes/recipeRoutes');
+
+const app = express();
+const PORT = 5000;
+
+const client = new MongoClient(
+    db.url,
+    {
+        useUnifiedTopology: true,
+        useNewUrlParser: true
+    }                        
+);
+
+async function main() {
+    try {
+        await client.connect();
+
+        // Здесь middleware's
+        app.use(express.json());
+        app.use(cors());
+
+        // Здесь пути для API
+        app.use('/api/v1/recipes', recipeRoutes(client.db()));
+
+        app.listen(PORT, () => {
+            console.log(`Server is running on port ${PORT}`)
+        });
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+main().catch(console.error);
---
+++ backend/routes/recipeRoutes.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const { ObjectId } = require('mongodb');
+
+const recipeRoutes = (db) => {
+    const router = express.Router();
+    const collection = db.collection('recipes');
+
+    router.get('/', async (req, res) => {
+        try {
+            const users = await collection.find({}).toArray();
+            res.status(200).json(users);
+        } catch (error) {
+            res.status(500).json({ error: error.message });
+        };
+    });
+
+    router.get('/:id', async (req, res) => {
+        try {
+            const user = await collection.findOne({ _id: new ObjectId(req.params.id) });
+            if (user) {
+                res.status(200).json(user);
+            } else {
+                res.status(404).json({ error: 'No recipte found' });
+            }
+        } catch (error) {    
+            res.status(500).json({ error: error.message });
+        };
+    });
+
+    router.post('/', async (req, res) => {
+        try {
+            const result = await collection.insertOne(req.body);
+            res.status(201).json(result.ops[0]);
+        } catch (error) {
+            res.status(500).json({ error: error.message });
+        };
+    });
+
+    router.delete('/:id', async (req, res) => {
+        try {
+            const result = await collection.deleteOne({ _id: new ObjectId(req.params.id) });
+            res.status(200).json(result);
+        } catch (error) {
+            res.status(500).json({ error: error.message });
+        };
+    });
+
+    routerter.put('/:id', async (req, res) => {
+        try {
+            const result = await collection.updateOne({ _id: new ObjectId(req.params.id) }, { $set: req.body });
+            res.status(200).json(result);
+        } catch (error) {
+            res.status(500).json({ error: error.message });
+        };
+    });
+
+    return router;
+};
+
+module.exports = recipeRoutes;
