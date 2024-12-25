751fcca4fb8e68383f9ff83b427918ae036a17a9
matyushkovvv <anmatyushkov@yandex.ru>
2024-12-25 04:23:47 +0300

    fix bag with database

--- backend/database.js
+++ backend/database.js
@@ -1,3 +1,4 @@
 module.exports = {
-    url: 'mongodb+srv://stagulmovir1:iduUSEHVaNVF1AV6@cookit.3qrvi.mongodb.net/'
+    url: 'mongodb+srv://stagulmovir1:iduUSEHVaNVF1AV6@cookit.3qrvi.mongodb.net/',
+    name: 'Cookit'
 }
--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const { MongoClient } = require('mongodb');
 const db = require('./database');
 const recipeRoutes = require('./routes/recipeRoutes');
-const productRouter = require('./routes/productRouter');
+const photoProductRouter = require('./routes/photoProductRouter');
 
 const app = express();
 const PORT = 5000;
@@ -19,14 +19,15 @@ const client = new MongoClient(
 async function main() {
     try {
         await client.connect();
+        const database = client.db(db.name);
 
         // Здесь middleware's
         app.use(express.json());
         app.use(cors());
 
         // Здесь пути для API
-        app.use('/api/v1/recipes', recipeRoutes(client.db()));
-        app.use('/api/v1/products', productRouter(client.db()));
+        app.use('/api/v1/recipes', recipeRoutes(database));
+        app.use('/api/v1/products', photoProductRouter(database));
 
         app.listen(PORT, () => {
             console.log(`Server is running on port ${PORT}`)
